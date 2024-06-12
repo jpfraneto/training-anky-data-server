a0f32285a8bb8d356d1f165c5f108f6f0adfecc5
anky faundez soto pena <anky@ankys-MacBook-Air.local>
2024-06-12 15:19:59 -0400

    alojjaa

--- lib/farcaster.ts
+++ lib/farcaster.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
-console.log("processss", process.env.NEYNAR_API_KEY)
 
 export  async function getThisCastInformationFromHash (castHash) {
     try {
+        console.log("processss", process.env.NEYNAR_API_KEY)
+
       const castResponse = await axios.get(`https://api.neynar.com/v2/farcaster/cast?identifier=${castHash}&type=hash&viewer_fid=16098`, {
         headers: {
           api_key: process.env.NEYNAR_API_KEY,
@@ -17,6 +18,8 @@ export  async function getThisCastInformationFromHash (castHash) {
   
   export  async function getThisCastInformationFromUrl (castUrl) {
     try {
+        console.log("proc2essss", process.env.NEYNAR_API_KEY)
+
       const castResponse = await axios.get(`https://api.neynar.com/v2/farcaster/cast?identifier=${encodeURIComponent(castUrl)}&type=url&viewer_fid=16098`, {
         headers: {
           api_key: process.env.NEYNAR_API_KEY,
